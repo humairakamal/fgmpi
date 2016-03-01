ad564ee51a6097dbd3be0b19f7b2290c7b544c01
Humaira Kamal <humaira@cs.ubc.ca>
2016-03-01 14:31:09 -0800

    add another case for MPI_Initialized
    An additional case was needed when standard MPI code (i.e. with a main()
    function without including any boilerplate code) is run under FG-MPI
    (nfg defaults to 1 if it's omitted on the command line).
    It is also possible to launch a mix of MPI-only code and FG-MPI
    code using the colon operator for mpiexec on the command line.
    FGP_ALL_POST_INIT now refers to the case when FGmpiexec() is called
    in the code for nfg >=1

--- src/mpi/init/initialized.c
+++ src/mpi/init/initialized.c
@@ -70,11 +70,9 @@ int MPI_Initialized( int *flag )
     /* ... body of routine ...  */
 
 #if defined(FINEGRAIN_MPI)
-    *flag = (FGP_init_state == FGP_ALL_POST_INIT
-             || (FGP_init_state == FGP_POST_INIT
-                 && 0 != CO_CURRENT->statevars))
-            && (IS_SPAWNER!=1);
-    
+    *flag = (0 != CO_CURRENT->statevars)
+             && (((FGP_init_state == FGP_ALL_POST_INIT) && (IS_SPAWNER != 1))
+                   || (FGP_init_state == FGP_POST_INIT));
 #else
     *flag = (OPA_load_int(&MPIR_Process.mpich_state) >= MPICH_POST_INIT);
 #endif
